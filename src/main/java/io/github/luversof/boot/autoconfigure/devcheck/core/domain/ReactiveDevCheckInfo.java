6c458d22b9438454281dd3db57d0f0b0bda35d9e
bluesky <bluesky@ncsoft.com>
2022-10-26 08:46:52 +0900

    Fix url link error when contextPath is set #8

--- src/main/java/io/github/luversof/boot/autoconfigure/devcheck/core/domain/ReactiveDevCheckInfo.java
+++ src/main/java/io/github/luversof/boot/autoconfigure/devcheck/core/domain/ReactiveDevCheckInfo.java
@@ -12,12 +12,12 @@ import io.github.luversof.boot.autoconfigure.devcheck.core.util.DevCheckUtil;
 
 public class ReactiveDevCheckInfo extends AbstractDevCheckInfo<RequestMappingInfo> {
 
-	public ReactiveDevCheckInfo(String pathPrefix, Entry<RequestMappingInfo, HandlerMethod> handlerMethodMap) {
+	public ReactiveDevCheckInfo(String contextPath, String pathPrefix, Entry<RequestMappingInfo, HandlerMethod> handlerMethodMap) {
 		setBeanName(handlerMethodMap.getValue().getBean().toString());
 		setUrlList(new ArrayList<>());
 		var patternsCondition = handlerMethodMap.getKey().getPatternsCondition();
 		for (PathPattern pathPattern : patternsCondition.getPatterns()) {
-			getUrlList().add(DevCheckUtil.getUrlWithParameter(pathPrefix, pathPattern.getPatternString(), handlerMethodMap.getValue().getMethod()));
+			getUrlList().add(DevCheckUtil.getUrlWithParameter(contextPath, pathPrefix, pathPattern.getPatternString(), handlerMethodMap.getValue().getMethod()));
 		}
 		setHandlerMethodMap(handlerMethodMap);
 		var methodAnnotation = handlerMethodMap.getValue().getMethodAnnotation(DevCheckDescription.class);
