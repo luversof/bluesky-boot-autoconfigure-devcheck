2a27aba81488362c87fdf0b5bb70b2866f610c70
bluesky <bluesky@ncsoft.com>
2023-06-08 10:30:26 +0900

    Apply AOP to allow devcheck requests based on the enabled option #9 close

---
+++ src/main/java/io/github/luversof/boot/autoconfigure/devcheck/core/exception/DevCheckException.java
@@ -0,0 +1,7 @@
+package io.github.luversof.boot.autoconfigure.devcheck.core.exception;
+
+public class DevCheckException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
